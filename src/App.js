40627fa237f690a92bbaaf781cb8a14c38fc1b1c
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-02 08:56:02 -0500

    Figured out how to open new tabs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <Tile name="Click" />
-      <Tile name="Other Environment" />
 
       {/* This is just here to show that this class works; and it prevents the console from throwing a fit.  Use it to debug the card that shows info for each environment, later. */}
       <InfoCard />
