922a3d4e2414bcadc6a36d8e79b28afa1ca8202e
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-07 12:23:03 -0500

    Formatted InfoCard Formatted InfoCard component to contain a list of fields for each various environment.  Still working on rendering that card on a new page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Tile from "./Components/Tile/Tile";
+import EnvTile from "./Components/EnvTile/EnvTile";
 import "./styles.css";
 import InfoCard from "./Components/InfoCard/InfoCard";
 
 export default function App() {
   return (
     <div className="App">
-      <Tile name="Click" />
+      <EnvTile id="Click Environments" />
 
       {/* This is just here to show that this class works; and it prevents the console from throwing a fit.  Use it to debug the card that shows info for each environment, later. */}
       <InfoCard />
---
+++ src/Components/EnvTile/EnvTile.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+import "./EnvTileStyle.css";
+
+export class EnvTile extends Component {
+  render() {
+    // Couldn't figure out how to get info to display on a new page, so I tested the button's functionality with an alert.
+
+    function sayHello() {
+      alert("You clicked me; my name is " + this.props.name + "!");
+      // console.log(this);  //Used for debugging; comment out once you figure out how to bind 'this' keyword.
+    }
+
+    function openTab() {
+      window.open("https://www.google.com#");
+    }
+
+    return (
+      <div className="Tile">
+        <div className="TileTitle">{this.props.id}</div>
+
+        {/* To bind an event handler, simply add '.bind(this)' to the function call. */}
+        <button className="TileButton" onClick={sayHello.bind(this)}>
+          Say Hello
+        </button>
+
+        {/* FIXME: Alternate approach to binding an event handler; using arrow functions, then calling the event handler in the arrow function body */}
+        {/* Left off at 6:38 on this video: https://www.youtube.com/watch?v=kVWpBtRjkCk */}
+        {/* <button className="TileButton" onClick={sayHello.bind(this)}>
+          View
+        </button> */}
+
+        {/* This is just me playing with opening tabs and new pages */}
+        <button className="TileButton" onClick={openTab.bind(this)}>
+          New Tab
+        </button>
+
+        <div />
+      </div>
+    );
+  }
+}
+
+export default EnvTile;
