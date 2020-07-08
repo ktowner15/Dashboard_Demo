7b1c1dee25f9cfa5e3e2f50abff0c537c63093da
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-08 13:48:03 -0500

    Messing around with links and react-router Trying to get the router package up and running; it's proving more difficult than originally thought.  This commit is  to save work done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import EnvTile from "./Components/EnvTile/EnvTile";
 import "./styles.css";
 import InfoCard from "./Components/InfoCard/InfoCard";
@@ -6,9 +7,13 @@ import InfoCard from "./Components/InfoCard/InfoCard";
 export default function App() {
   return (
     <div className="App">
-      <EnvTile id="Click Environments" />
+      {/* The always-visible JSX/HTML is declared here, as well as router links. Examples below:*/}
+      {/* <EnvTile id="Click Environments" /> */}
+      {/* <Link to="/">Home</Link> */}
 
-      {/* This is just here to show that this class works; and it prevents the console from throwing a fit.  Use it to debug the card that shows info for each environment, later. */}
+      <EnvTile id="Click" />
+
+      {/* TODO: get this to render on a different page */}
       <InfoCard />
     </div>
   );
