fde28df771c16054593371e110667c3812052100
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-09 09:48:27 -0500

    Cleaned up Got rid of extra unneeded JSX elements and cleaned up functions so they rendered as fragments.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import EnvTile from "./Components/EnvTile/EnvTile";
@@ -8,22 +8,21 @@ import InfoCard from "./Components/InfoCard/InfoCard";
 export default function App() {
   return (
     <Router>
-      <main>
-        <div className="App">
-          {/* The always-visible JSX/HTML is declared here, as well as router links. Examples below:*/}
-          {/* <EnvTile id="Click Environments" /> */}
-          {/* <Link to="/">Home</Link> */}
+      <div className="App">
+        {/* The always-visible JSX/HTML is declared here, as well as router links. Examples below:*/}
+        {/* <EnvTile id="Click Environments" /> */}
+        {/* <Link to="/">Home</Link> */}
 
-          <EnvTile id="Click">
-            {/* TODO: perhaps experiment by putting link here? */}
-          </EnvTile>
+        <EnvTile appName="Click">
+          {/* TODO: perhaps experiment by putting link here? */}
+        </EnvTile>
 
-          {/* TODO: get this link to open on a separate page, that way you don't need the 'close' link. */}
-          <Link to="/information">Information</Link>
-          <text> || </text>
-          <Link to="/">Close</Link>
-        </div>
-      </main>
+        {/* TODO: get this link to open on a separate page, that way you don't need the 'close' link. */}
+        <Link to="/information">Information</Link>
+        <text> || </text>
+        <Link to="/">Close</Link>
+      </div>
+
       <Switch>
         <Route path="/" exact component={blank} />
         <Route path="/information" component={infoDock} />
@@ -32,6 +31,15 @@ export default function App() {
   );
 }
 
-const blank = () => <div />;
+// Link elements go below here.
+const blank = () => (
+  <Fragment>
+    <div />
+  </Fragment>
+);
 
-const infoDock = () => <InfoCard />;
+const infoDock = () => (
+  <Fragment>
+    <InfoCard />
+  </Fragment>
+);
