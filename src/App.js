758f607e941951ba2e298187240a7ccca576e246
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-09 14:58:17 -0500

    Got react-router working inside of app tiles Got react-router working inside of app tiles.  Didn't have much time today; I'll try to work more on this after a meeting that starts in about 5 minutes.  If there's not time, then this will be my last commit for today.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from "react";
 
 import EnvTile from "./Components/EnvTile/EnvTile";
 import "./styles.css";
-import InfoCard from "./Components/InfoCard/InfoCard";
 
 export default function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* The always-visible JSX/HTML is declared here, as well as router links. Examples below:*/}
-        {/* <EnvTile id="Click Environments" /> */}
-        {/* <Link to="/">Home</Link> */}
+    <div className="App">
+      {/* The always-visible JSX/HTML is declared here, as well as router links. Examples below:*/}
+      {/* <EnvTile id="Click Environments" /> */}
+      {/* <Link to="/">Home</Link> */}
 
-        <EnvTile appName="Click">
-          {/* TODO: perhaps experiment by putting link here? */}
-        </EnvTile>
-
-        {/* TODO: get this link to open on a separate page, that way you don't need the 'close' link. */}
-        <Link to="/information">Information</Link>
-        <text> || </text>
-        <Link to="/">Close</Link>
-      </div>
-
-      <Switch>
-        <Route path="/" exact component={blank} />
-        <Route path="/information" component={infoDock} />
-      </Switch>
-    </Router>
+      <EnvTile appName="Click" />
+      <EnvTile appName="App2" />
+    </div>
   );
 }
-
-// Link elements go below here.
-const blank = () => (
-  <Fragment>
-    <div />
-  </Fragment>
-);
-
-const infoDock = () => (
-  <Fragment>
-    <InfoCard />
-  </Fragment>
-);
