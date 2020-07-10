dc1aa718e65a6c40a45cee12a33967c3721982e9
ktowner15 <33582642+ktowner15@users.noreply.github.com>
2020-07-10 12:08:16 -0500

    Finally got the redirect working The redirect to the raw HTML page is working, at last.  Now to fill it with data.

--- src/Components/EnvTile/EnvTile.js
+++ src/Components/EnvTile/EnvTile.js
@@ -1,49 +1,63 @@
 import React, { Component } from "react";
 
 import "./EnvTileStyle.css";
-import InfoCard from "../InfoCard/InfoCard";
+
+import LinkBar from "../LinkBar/LinkBar";
+import infoPage from "../rawHTML/infoPage.html";
 
 export class EnvTile extends Component {
+  redirect() {
+    // hide the app tiles and replace them with the app's information.  Seems like a redirect, but it's not.
+    // this.document.getElementById("applications").innerHTML = (
+    //   <div> Hello World </div>
+    // );
+    // console.log(document.getElementById("applications").innerHTML);
+
+    // window.open(infoPage);
+    console.log(document.getRootNode().body.innerHTML);
+    document.getRootNode().body.innerHTML = infoPage;
+  }
+
   render() {
     // Insert JS functions here
 
-    // Couldn't figure out how to get info to display on a new page, so I tested the button's functionality with an alert.
+    //playing around with alerts
+    // function sayHello() {
+    //   alert("You clicked me; my name is " + this.props.id + "!");
+    //   // console.log(this);  //Used for debugging; comment out once you figure out how to bind 'this' keyword.
+    // }
 
-    function sayHello() {
-      alert("You clicked me; my name is " + this.props.id + "!");
-      // console.log(this);  //Used for debugging; comment out once you figure out how to bind 'this' keyword.
-    }
-
-    function openTab() {
-      window.open("https://www.google.com#");
-    }
+    //playing around with opening a new tab
+    // function openTab() {
+    //   window.open("https://www.google.com#");
+    // }
 
     return (
       <div className="Tile">
-        <div className="TileTitle">{this.props.id}</div>
+        <div className="TileTitle">{this.props.appName}</div>
+
+        <button onClick={this.redirect}>Redirect</button>
 
         {/* To bind an event handler, simply add '.bind(this)' to the function call. */}
-        <button className="TileButton" onClick={sayHello.bind(this)}>
+        {/* <button className="TileButton" onClick={sayHello.bind(this)}>
           Say Hello
-        </button>
+        </button> */}
 
         {/* FIXME: Alternate approach to binding an event handler; using arrow functions, then calling the event handler in the arrow function body */}
         {/* Left off at 6:38 on this video: https://www.youtube.com/watch?v=kVWpBtRjkCk */}
         {/* <button className="TileButton" onClick={sayHello.bind(this)}>
           View
-        </button> */}
+          </button> */}
 
         {/* This is just me playing with opening tabs and new pages */}
-        <button className="TileButton" onClick={openTab.bind(this)}>
-          New Tab
-        </button>
+        {/* <button className="TileButton" onClick={openTab.bind(this)}>
+            New Tab
+          </button> */}
 
         <div />
 
-        {/* TODO: use the react-router-dom pkg to render InfoCards on a separate page. */}
-        <div>
-          <InfoCard className="TileCard" />
-        </div>
+        {/* Here is the component that contains the links to information about the environment */}
+        <LinkBar className="InfoLink" />
       </div>
     );
   }
